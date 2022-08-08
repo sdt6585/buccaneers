fa7d9cd15bfaee3db057ce7fa9b5e4178547e379
BuildTools <unconfigured@null.spigotmc.org>
2022-08-08 17:38:57 -0500

    Passport implemented with magic link option

--- client/app.js
+++ client/app.js
@@ -1,47 +1,91 @@
-/************************************************ Utility function for getting route parameters */
-function getRouteParams() {
-  
-  //Grab any parameters passed in the route
-  let params = {},
-    paramParts = window.location.hash.split('/')
-  
-  if (paramParts.length > 1) {
-    //Grab paramater key/value pairs ie - site.com#campaign/campaign_id/1 saves an object of {campaign_id: '1'}
-    for (let i = 1; i < paramParts.length; i = i + 2) {
-      params[paramParts[i]] = paramParts[i+1];
-    }
+//Just a wrapper function to allow us to use async/await
+(async function () {
+  /*********************************************************************************Setup App - Shared variables and an object for sharing when we grow beyond one javascript page */
+  let app = {};
+
+  //Check if we're logged in if we're not on a login path or the home page
+  if (window.location.hash.split('/')[0] !== '#magic_link' && window.location.hash !== '#') {
+    //Grab the user
+    try {
+      app.user = (await (await fetch(`./api/user`)).json()).data[0];
+    } catch (e) {
+      window.location.hash = 'not-authorized';
+    }    
   }
 
-  return params;
-}
-/************************************************ Initialize the Router */
-const route = Rlite(notFound, {
-  // Default route
-  '': notFound(),
+  /********************************************************************************* Utility function for getting route parameters */
+  function getRouteParams() {
+    
+    //Grab any parameters passed in the route
+    let params = {},
+      paramParts = window.location.hash.split('/')
+    
+    if (paramParts.length > 1) {
+      //Grab paramater key/value pairs ie - site.com#campaign/campaign_id/1 saves an object of {campaign_id: '1'}
+      for (let i = 1; i < paramParts.length; i = i + 2) {
+        params[paramParts[i]] = paramParts[i+1];
+      }
+    }
 
-  'campaigns': showCampaigns,
-});
+    return params;
+  }
+  /********************************************************************************* Initialize the Router */
+  const route = Rlite(notFound, {
+    '': showHome(),
+    'campaigns': showCampaigns,
+    'campaign': showCampaign,
+    'magic_link': magicLinkLogin
+  });
 
-function notFound() {
-  document.body.innerHTML = '<h1>404 Not found :/</h1>';
-}
+  function notFound() {
+    document.body.innerHTML = '<h1>404 Not found :/</h1>';
+  }
 
-// Hash-based routing
-function processHash() {
-  const hash = location.hash || '#';
+  // Hash-based routing
+  function processHash() {
+    const hash = location.hash || '#';
 
-  // Route based on the first route part
-  route(hash.split('/')[0].slice(1));
-}
+    // Route based on the first route part
+    route(hash.split('/')[0].slice(1));
+  }
 
-window.addEventListener('hashchange', processHash);
-processHash();
+  window.addEventListener('hashchange', processHash);
+  processHash();
 
-async function showCampaigns () {
-  let params = getRouteParams();
+  /********************************************************************************* Magic Link Login */
+  async function magicLinkLogin () {
+    try {
+      let params = getRouteParams();
 
-  if (params.user_id) {
-    let request = await fetch(`./api/user/${params.user_id}/campaigns/`);
+      app.user = await (await fetch(`./api/login`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: 'MagicLink!!!!!!!!!!!!!!!!!!!!!', password: params.magic_link})
+      })).json();
+
+      location.hash = 'campaigns';
+    } catch (e) {
+      notFound();
+    }  
+  }
+
+  /********************************************************************************* Show Campaigns */
+
+  async function showHome () {
+
+    document.body.innerHTML =  '<h1>Buccaneers Booty</h1>';  
+    document.body.innerHTML += '<p>To use this tool you must receive an invitation from a game master.  Check back later to check for details on a public beta test of the tool</p>';  
+  }
+
+  /********************************************************************************* Show Campaigns */
+
+  async function showCampaigns () {
+    let params = getRouteParams();
+
+    let request = await fetch(`./api/user/campaigns`);
     let response = await request.json();
 
     if (response.success) {
@@ -54,14 +98,47 @@ async function showCampaigns () {
     } else {
       document.body.innerHTML = `There was an error :-(<br/>${response.message}`;
     }
-  } else {
-    document.body.innerHTML = `
-      You must have a valid user id to view campaigns, please ask your game master for a link that contains yours
-    `;
+    
   }
-  
-}
 
-function showCampaign(options) {
 
-}
+  /********************************************************************************* Show Campaign/List Characters */
+
+  async function showCampaign() {
+    let params = getRouteParams();
+
+    if (params.campaign_id) {
+      let campaignResponse = await (await fetch(`./api/campaign/${params.campaign_id}`)).json();
+
+      if (campaignResponse.success && characterResponse.success) {
+        //Grab/filter the data
+        let campaign = campaignResponse.data[0],
+          characters = _.groupBy(characterResponse.data, 'user_id'),
+          userCharacters = characters[parseInt(params.user_id)],
+          activeCharacter = _.first(characters, {user_id: parseInt(params.user_id), is_active: 1});
+
+        //Clear the target area and show the campaign name
+        document.body.innerHTML = `
+          <h1>Campaign: ${campaign.name}}</h1>
+          <h3>User: ${campaign.name}</h3>
+        `;
+
+        //Show the active character
+        document.body.innerHTML += '<';
+
+
+        for (let character of response.data) {
+          document.body.innerHTML += `<a href='#campaign/campaign_id/${campaign.campaign_id}'>${campaign.name}</a><br/>`;
+        } 
+
+          document.body.innerHTML += `<a href='#campaign/campaign_id/${campaign.campaign_id}'>${campaign.name}</a><b+r/><`;
+      } else {
+        document.body.innerHTML = `There was an error :-(<br/>${campaignReponse.message || characterResponse.message}`;
+      }
+    } else {
+      document.body.innerHTML = `
+        You must have a valid user id to view campaigns, please ask your game master for a link that contains yours
+      `;
+    }
+  }
+})();
--- index.js
+++ index.js
@@ -14,24 +14,97 @@ const pool = mysql.createPool({
 });
  
 /******************************************* Configure Express/Server */
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const app = express();
+const port = 80;
 
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.disable('etag'); //disables caching - TODO - remove this/find a way to disable only for api requests
+const expiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 1 hour
+app.use(session({
+  name: 'bucaneerSessionId',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: '14onpr2c124301f39oijf390f39j0f34u1h9iu149831h8913',
+  cookie: { 
+    //secure: true 
+  }
+}));
+
+//Setup passport for authentication stuff later and tracking user now
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy(async function (username, password, done) {
+  try {
+    if (username === 'MagicLink!!!!!!!!!!!!!!!!!!!!!') {
+      
+      const connection = pool.promise();
+
+      let user = (await connection.query(`
+        SELECT *
+        FROM user 
+        WHERE user_id = ${connection.escape(password)}
+      `))[0][0];
+      
+      //If we found the user, great!
+      if (user) {
+        //Success - return the user
+        done(null, user);
+      } else {
+        //Return our custom error message
+        done(null, false, 'You are not authorized');
+      }
+    } else {
+      //TODO - Implement username/password here or google oauth in a separate strategy, or both
+    }
+} catch (e) {
+  //Return the unknown error
+  done(e);
+}
+}));
+
+app.use(passport.authenticate('session'));
+
+//Serve client html from the client folder
 app.use(express.static('client'))
 
+/******************************************* User Login*/
+app.post('/api/login', passport.authenticate('local', { failureMessage: true }), function(req, res) {
+  res.send(req.session.passport.user);
+});
+
 /******************************************* Campaigns */
-app.get('/api/user/:user_id/campaigns', async function (req, res) {
+app.get('/api/user/:user_id/campaigns', getCampaigns);
+app.get('/api/user/campaigns', getCampaigns);
 
+async function getCampaigns (req, res) {
   try { 
+    console.log(req.params.user_id || req.session.passport.user.user_id);
     const connection = pool.promise();
 
     let results = await connection.query(`
-      SELECT 
-        campaign.* 
+        SELECT 
+        campaign.*
       FROM campaign 
-        INNER JOIN user_campaign USING (campaign_id) 
-      WHERE user_campaign.user_id = ${connection.escape(req.params.user_id)}
+        INNER JOIN user_campaign ON (user_campaign_id)
+        INNER JOIN user USING (user_id)
+      WHERE campaign.campaign_id = ${connection.escape(req.params.user_id || req.session.passport.user.user_id)}
     `);
     
     res.send({
@@ -45,22 +118,63 @@ app.get('/api/user/:user_id/campaigns', async function (req, res) {
       message: e.message 
     });
   }
-});
+}
 
 
 
-/******************************************* Characters */
-app.get('/api/campaign/:campaign_id/characters', async function (req, res) {
+/******************************************* Campaign Detail */
+app.get('/api/campaign/:campaign_id', async function (req, res) {
 
   try { 
     const connection = pool.promise();
-
-    let results = await connection.query(`
+    
+    let campaign = (await connection.query(`
       SELECT 
-        character.*
+        *
       FROM campaign 
-        INNER JOIN character USING (campaign_id) 
+      WHERE campaign_id = ${connection.escape(req.params.campaign_id)}
+    `))[0][0];
+
+    campaign.characters = (await connection.query(`
+      SELECT 
+        \`character\`.*,
+        user.name AS user_name,
+        user.user_id,
+        user_campaign.user_campaign_id,
+        user_campaign.active_character_id
+      FROM campaign 
+        INNER JOIN \`character\` USING (campaign_id) 
+        INNER JOIN user_campaign USING (user_campaign_id)
+        INNER JOIN user USING (user_id)
       WHERE campaign.campaign_id = ${connection.escape(req.params.campaign_id)}
+    `))[0]
+    
+    res.send({
+      success: true,
+      data: campaign
+    });
+
+  } catch (e) {
+    res.send({
+      success: false,
+      message: e.message 
+    });
+  }
+});
+
+/******************************************* User */
+app.get('/api/user/:user_id', getUser);
+app.get('/api/user', getUser); //returns the currently logged in user
+async function getUser (req, res) {
+
+  try { 
+    const connection = pool.promise();
+
+    let results = await connection.query(`
+      SELECT 
+        * 
+      FROM user 
+      WHERE user_id = ${connection.escape(req.params.user_id || req.session.passport.user.user_id)}
     `);
     
     res.send({
@@ -74,7 +188,7 @@ app.get('/api/campaign/:campaign_id/characters', async function (req, res) {
       message: e.message 
     });
   }
-});
+}
 
 
 /********************************************** Start Server */
